265570a189c16419418d17955be64bb6ff7945e2
InnaL <innalapteva72@gmail.com>
2020-01-31 11:52:50 +0500

    Initial Commit

---
+++ SB_HW2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SB_HW2
+//
+//  Created by Manager on 31/01/2020.
+//  Copyright © 2020 Inna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
