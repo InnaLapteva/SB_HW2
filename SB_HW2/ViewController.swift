093ece0dbd1e26dcc773ee77c95a739cff4419f3
InnaL <innalapteva72@gmail.com>
2020-01-31 16:13:42 +0500

    textFields work

--- SB_HW2/ViewController.swift
+++ SB_HW2/ViewController.swift
@@ -19,6 +19,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
     var currentRed: Float = 0.4
     var currentBlue: Float = 0.8
     var currentGreen: Float = 0.2
@@ -38,7 +42,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
         updateViewColor(view: colorView)
         
         super.viewDidLoad()
+        
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
     }
+ 
     
     @IBAction func redSliderChanged() {
         currentRed = redSlider.value
@@ -55,7 +64,49 @@ class ViewController: UIViewController, UITextFieldDelegate {
         sliderWasChanged(slider: blueSlider, slidersLabel: greenLabel, slidersColor: currentBlue)
     }
     
-    //MARK:
+   
+    //MARK: UITextFieldDelegate
+    //скрываем клавиатуру при нажатии кнопки
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+           textField.resignFirstResponder()
+           return true
+       }
+    //скрываем клавиатуру при тапу на экран
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        view.endEditing(true)
+    }
+  
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        guard let text = textField.text else { return }
+               
+               if let currentValue = Float(text) {
+                   
+                   switch textField.tag {
+                   case 0:
+                    redSlider.value = currentValue
+                    currentRed = currentValue
+                    redLabel.text = String(currentValue)
+                   case 1:
+                    greenSlider.value = currentValue
+                    currentGreen = currentValue
+                    greenLabel.text = String(currentValue)
+                   case 2:
+                    blueSlider.value = currentValue
+                    currentBlue = currentValue
+                    blueLabel.text = String(currentValue)
+                   default: break
+                }
+                
+               } else {
+                    self.showAlert(with: "Wrong format", and: "Please enter in 0.00")
+                }
+                
+                 updateViewColor(view: colorView)
+    }
+    //MARK: function
     
     func showSlider(slider: UISlider, min: Float, max: Float, value: Float, color: UIColor) {
         slider.minimumValue = min
@@ -71,6 +122,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func sliderWasChanged(slider: UISlider, slidersLabel: UILabel, slidersColor: Float) {
         slidersLabel.text = String(format: "%02.2f", slidersColor)
         updateViewColor(view: colorView)
+    }
+    
+    func showAlert(with title: String, and message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .cancel)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+        
     }
     
 }
